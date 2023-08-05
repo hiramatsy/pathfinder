dc63963b324f4b08cd0cbe28a841a04b7cf4b6fc
hiramatsy <hiramatsy@outlook.jp>
2023-08-05 21:54:10 +0900

    [pathfinder] Color the pathway

--- 2/ui.js
+++ 2/ui.js
@@ -1,15 +1,23 @@
-const renderGrid = (w, h, parentEl) => {
+const renderGrid = (w, h, parentEl, path) => {
     const grid = document.createElement('div');
     grid.classList.add('grid');
+    const gridCellElements = [];
     for (let i = 0; i < h; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
+        const rowElements = [];
         for (let j = 0; j < w; j++) {
             const cell = document.createElement('span');
-            cell.classList.add('cell');
+            cell.classList.add('cell', 'cell-unused');
             row.appendChild(cell);
+            rowElements.push(cell);
         }
         grid.appendChild(row);
+        gridCellElements.push(rowElements);
     }
     parentEl.appendChild(grid);
+
+    for (const { x, y } of path) {
+        gridCellElements[y][x].classList.replace('cell-unused', 'cell-used');
+    }
 };
