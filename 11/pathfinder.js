fbed6403124f72df37cc47bb43af3c8ceb896578
hiramatsy <hiramatsy@outlook.jp>
2023-08-09 00:08:13 +0900

    [pathfinder] Checkin base source files

---
+++ 11/pathfinder.js
@@ -0,0 +1,188 @@
+const CELL_UNUSED = 0;
+const CELL_USED = 1;
+const CELL_NONSTEPPABLE = 2;
+
+const step_r = (w, h, currentpos, goal, gridStatus, path, result_paths) => {
+    for (const [dx, dy] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+        const nx = currentpos.x + dx;
+        const ny = currentpos.y + dy;
+
+        if (nx < 0 || nx >= w || ny < 0 || ny >= h) {
+            // out of area
+            continue;
+        }
+
+        if (gridStatus[ny][nx] !== CELL_UNUSED) {
+            // already trod
+            continue;
+        }
+
+        if (nx === goal.x && ny === goal.y) {
+            // reach the goal
+            const okpath = path.slice();
+            okpath.push(goal);
+            result_paths.push(okpath);
+            continue;
+        }
+
+        const nextCurrentPos = { x: nx, y: ny };
+        path.push(nextCurrentPos)
+        gridStatus[ny][nx] = CELL_USED;
+        step_r(w, h, nextCurrentPos, goal, gridStatus, path, result_paths);
+        path.pop();
+        gridStatus[ny][nx] = CELL_UNUSED;
+    }
+}
+
+const step_itr = (w, h, start, goal, gridStatus, path, result_paths) => {
+    const dirs = [];
+    const diffs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    const currPos = structuredClone(start);
+    let currentDir = 0;
+
+    const popState = () => {
+        const popped = path.pop();
+        gridStatus[popped.y][popped.x] = CELL_UNUSED;
+        currPos.x = path[path.length - 1].x;
+        currPos.y = path[path.length - 1].y;
+        return dirs.pop();
+    }
+
+    const nextDir = () => {
+        if (currentDir < diffs.length - 1) {
+            currentDir++;
+            return true;
+        } else {
+            for (;;) {
+                if (dirs.length === 0) {
+                    return false;
+                }
+                const lastDir = popState();
+                if (lastDir < diffs.length - 1) {
+                    currentDir = lastDir + 1;
+                    return true;
+                }
+            }
+        }
+    }
+
+    for (;;) {
+        const [dx, dy] = diffs[currentDir];
+        const nx = currPos.x + dx;
+        const ny = currPos.y + dy;
+
+        if (nx < 0 || nx >= w || ny < 0 || ny >= h) {
+            // out of area
+            if (!nextDir()) {
+                return;
+            }
+            continue;
+        }
+
+        if (gridStatus[ny][nx] !== CELL_UNUSED) {
+            // already trod
+            if (!nextDir()) {
+                return;
+            }
+            continue;
+        }
+
+        if (nx === goal.x && ny === goal.y) {
+            // reach the goal
+            const okpath = path.slice();
+            okpath.push(goal);
+            result_paths.push(okpath);
+            if (!nextDir()) {
+                return;
+            }
+            continue;
+        }
+
+        currPos.x = nx;
+        currPos.y = ny;
+        path.push(structuredClone(currPos));
+
+        dirs.push(currentDir);
+        currentDir = 0;
+
+        gridStatus[ny][nx] = CELL_USED;
+    }
+}
+
+const findUsingBFS = (w, h, start, goal, gridStatus, path, result_paths) => {
+    const diffs = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    const ctxQueue = [[start, gridStatus, path]];
+
+    for (; ;) {
+        if (ctxQueue.length === 0) {
+            return;
+        }
+        const [cPos, cGrid, cPath] = ctxQueue.shift();
+        for (const [dx, dy] of diffs) {
+            const nx = cPos.x + dx;
+            const ny = cPos.y + dy;
+
+            if (nx < 0 || nx >= w || ny < 0 || ny >= h) {
+                // out of area
+                continue;
+            }
+
+            if (cGrid[ny][nx] !== CELL_UNUSED) {
+                // already trod
+                continue;
+            }
+
+            if (nx === goal.x && ny === goal.y) {
+                // reach the goal
+                const okpath = cPath.slice();
+                okpath.push(goal);
+                result_paths.push(okpath);
+                continue;
+            }
+
+            const nPos = { x: nx, y: ny };
+            const nGrid = structuredClone(cGrid);
+            nGrid[ny][nx] = CELL_USED;
+            const nPath = cPath.slice();
+            nPath.push(nPos);
+            ctxQueue.push([nPos, nGrid, nPath]);
+        }
+    }
+};
+
+const DFS_RECURSIVE = 0;
+const DFS_ITERATIVE = 1;
+const BFS = 2;
+
+const findPaths = (w, h, start, goal, nonsteppables, traverseStyle=DFS_RECURSIVE) => {
+    const result_paths = [];
+    const gridStates = Array(h).fill().map(() => Array(w).fill(CELL_UNUSED));
+    for (const { x, y } of nonsteppables) {
+        gridStates[y][x] = CELL_NONSTEPPABLE;
+    }
+    gridStates[start.y][start.x] = CELL_USED;
+    switch (traverseStyle) {
+        case DFS_RECURSIVE:
+            step_r(w, h, start, goal, gridStates, [start], result_paths);
+            break;
+        case DFS_ITERATIVE:
+            step_itr(w, h, start, goal, gridStates, [start], result_paths);
+            break;
+        case BFS:
+            findUsingBFS(w, h, start, goal, gridStates, [start], result_paths);
+            break;
+        default:
+            step_r(w, h, start, goal, gridStates, [start], result_paths);
+            break;
+    }
+
+    return result_paths;
+}
+
+if (!exports) {
+    var exports = {};
+}
+exports.DFS_RECURSIVE = DFS_RECURSIVE;
+exports.DFS_ITERATIVE = DFS_ITERATIVE;
+exports.BFS = BFS;
+exports.findPaths = findPaths;
---
+++ 11/ui.js
@@ -0,0 +1,335 @@
+const renderGrid = (w, h, parentEl, path, nonsteppables, start, goal) => {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    const gridCellElements = [];
+    for (let i = 0; i < h; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        const rowElements = [];
+        for (let j = 0; j < w; j++) {
+            const cell = document.createElement('span');
+            cell.classList.add('cell', 'cell-unused');
+            row.appendChild(cell);
+            rowElements.push(cell);
+        }
+        grid.appendChild(row);
+        gridCellElements.push(rowElements);
+    }
+    parentEl.appendChild(grid);
+
+    for (const [i, { x, y }] of path.entries()) {
+        gridCellElements[y][x].classList.replace('cell-unused', 'cell-used');
+        gridCellElements[y][x].innerText = i;
+    }
+
+    for (let pidx = 0; pidx < path.length - 1; pidx++) {
+        const p0 = path[pidx];
+        const p1 = path[pidx + 1];
+        if (p0.x === p1.x) {
+            if (p1.y - p0.y === 1) { // [0, 1]
+                gridCellElements[p0.y][p0.x].classList.add('cell-bottom-open');
+                gridCellElements[p1.y][p1.x].classList.add('cell-top-open');
+            } else if (p1.y - p0.y === -1) { // [0, -1]
+                gridCellElements[p0.y][p0.x].classList.add('cell-top-open');
+                gridCellElements[p1.y][p1.x].classList.add('cell-bottom-open');
+            }
+        } else if (p0.y === p1.y) {
+            if (p1.x - p0.x === 1) { // [1, 0]
+                gridCellElements[p0.y][p0.x].classList.add('cell-right-open');
+                gridCellElements[p1.y][p1.x].classList.add('cell-left-open');
+            } else if (p1.x - p0.x === -1) { // [-1, 0]
+                gridCellElements[p0.y][p0.x].classList.add('cell-left-open');
+                gridCellElements[p1.y][p1.x].classList.add('cell-right-open');
+            }
+        }
+    }
+
+    for (const { x, y } of nonsteppables) {
+        gridCellElements[y][x].classList.replace('cell-unused', 'cell-nonsteppable');
+    }
+
+    gridCellElements[start.y][start.x].classList.add('cell-start');
+    gridCellElements[goal.y][goal.x].classList.add('cell-goal');
+};
+
+const clearGrid = (parentEl) => {
+    Array.from(parentEl.querySelectorAll('div.grid')).forEach(el => el.remove());
+};
+
+const validateInput = (ivWh, ivStart, ivGoal, ivNonsteppables) => {
+    const [w, h] = ivWh.split(',').map(Number);
+    const [sX, sY] = ivStart.split(',').map(Number);
+    const [gX, gY] = ivGoal.split(',').map(Number);
+    const nonsteppables = ivNonsteppables.length === 0 ? [] : ivNonsteppables.split(' ').map((xy) => { const [x, y] = xy.split(',').map(Number); return { x, y }; });
+    console.log(w, h, sX, sY, gX, gY, nonsteppables);
+
+    if (w <= 0 || h <= 0) {
+        return {
+            ok: false,
+            message: `Grid size was invalid. ${x},${y}`,
+        }
+    }
+
+    if (sX >= w || sY >= h) {
+        return {
+            ok: false,
+            message: `Start is out of area. ${sX},${sY}`,
+        }
+    }
+
+    if (gX >= w || gY >= h) {
+        return {
+            ok: false,
+            message: `Goal is out of area. ${gX},${gY}`,
+        }
+    }
+
+    if (sX === gX && sY === gY) {
+        return {
+            ok: false,
+            message: `Start and goal is same. ${sX},${sY}`,
+        }
+    }
+
+    for (const [i, { x, y }] of nonsteppables.entries()) {
+        if (x >= w || y >= h) {
+            return {
+                ok: false,
+                message: `Some of nonsteppables is out of area. [${i}] = ${x},${y}`,
+            }
+        }
+    }
+
+    for (const [i, { x, y }] of nonsteppables.entries()) {
+        if (x === sX && y === sY) {
+            return {
+                ok: false,
+                message: `Some of nonsteppables is same as the start [${i}] = ${x},${y}`,
+            }
+        }
+    }
+
+    for (const [i, { x, y }] of nonsteppables.entries()) {
+        if (x === gX && y === gY) {
+            return {
+                ok: false,
+                message: `Some of nonsteppables is same as the goal [${i}] = ${x},${y}`,
+            }
+        }
+    }
+
+    return {
+        ok: true,
+        w,
+        h,
+        start: { x: sX, y: sY },
+        goal: { x: gX, y: gY },
+        nonsteppables: nonsteppables,
+    }
+}
+
+const renderUserInterface = (parentEl, gridEl) => {
+    const buttonClearGrids = document.createElement('button');
+    buttonClearGrids.innerText = 'Clear grids';
+    buttonClearGrids.addEventListener('click', () => {
+        clearGrid(document.body);
+    });
+
+    // Grid size
+    const labelGridSize = document.createElement('label');
+    labelGridSize.htmlFor = "input-gridsize";
+    labelGridSize.innerText = 'gridsize:';
+
+    const inputGridSize = document.createElement('input');
+    inputGridSize.type = 'text';
+    inputGridSize.pattern = "[1-9][0-9]*,[1-9][0-9]*"
+    inputGridSize.id = "input-gridsize";
+    inputGridSize.required = true;
+    inputGridSize.placeholder = "W,H";
+
+    // Start
+    const labelStart = document.createElement('label');
+    labelStart.htmlFor = "input-start";
+    labelStart.innerText = 'start:';
+
+    const inputStart = document.createElement('input');
+    inputStart.type = 'text';
+    inputStart.id = "input-start";
+    inputStart.pattern = "(0|[1-9][0-9]*),(0|[1-9][0-9]*)"
+    inputStart.required = true;
+    inputStart.placeholder = "x,y";
+
+    // Goal
+    const labelGoal = document.createElement('label');
+    labelGoal.htmlFor = "input-goal";
+    labelGoal.innerText = 'goal:';
+
+    const inputGoal = document.createElement('input');
+    inputGoal.type = 'text';
+    inputGoal.id = "input-goal";
+    inputGoal.pattern = "(0|[1-9][0-9]*),(0|[1-9][0-9]*)";
+    inputGoal.required = true;
+    inputGoal.placeholder = "x,y";
+
+    // Non-steppables
+    const labelNonsteppables = document.createElement('label');
+    labelNonsteppables.htmlFor = "input-nonsteppables";
+    labelNonsteppables.innerText = 'nonsteppables:';
+
+    const inputNonsteppables = document.createElement('input');
+    inputNonsteppables.type = 'text';
+    inputNonsteppables.id = "input-nonsteppables";
+    inputNonsteppables.pattern = "(0|[1-9][0-9]*),(0|[1-9][0-9]*)( (0|[1-9][0-9]*),(0|[1-9][0-9]*))*";
+    inputNonsteppables.required = false;
+    inputNonsteppables.placeholder = "x1,y1 x2,y2 x3,y3, ...";
+
+    const messageArea = document.createElement('div');
+
+    const buttonValidateInput = document.createElement('button');
+    buttonValidateInput.innerText = 'ValidateInput';
+
+    const buttonFindPaths = document.createElement('button');
+    buttonFindPaths.innerText = 'FindPaths';
+    buttonFindPaths.disabled = true;
+
+    // Sample game selection
+    const labelSelectSearchVariant = document.createElement('label');
+    labelSelectSearchVariant.htmlFor = "select-search-variant";
+    labelSelectSearchVariant.innerText = 'choose a search variant:';
+
+    const selectSearchVariant = document.createElement('select');
+    selectSearchVariant.id = "select-search-variant";
+    const SEARCH_VARIANTS = [
+        [DFS_RECURSIVE, "DFS_RECURSIVE"],
+        [DFS_ITERATIVE, "DFS_ITERATIVE"],
+        [BFS, "BFS"],
+    ];
+    for (const [sid, svar] of SEARCH_VARIANTS) {
+        const sopEl = document.createElement('option');
+        sopEl.value = String(sid);
+        sopEl.innerText = svar;
+        selectSearchVariant.add(sopEl);
+    }
+
+    // Sample game selection
+    const labelSelectSampleGame = document.createElement('label');
+    labelSelectSampleGame.htmlFor = "select-sample-game";
+    labelSelectSampleGame.innerText = 'Sample game selection:';
+
+    const selectSampleGame = document.createElement('select');
+    selectSampleGame.id = "select-sample-game";
+    selectSampleGame.addEventListener('change', (event) => {
+        const selectedValue = JSON.parse(event.target.options[event.target.selectedIndex].value);
+        if (selectedValue) {
+            const { w, h, nonsteppables, start, goal } = selectedValue;
+            inputGridSize.value = `${w},${h}`;
+            inputStart.value = `${start.x},${start.y}`;
+            inputGoal.value = `${goal.x},${goal.y}`;
+            inputNonsteppables.value = nonsteppables.map(({ x, y }) => `${x},${y}`).join(" ");
+        }
+    });
+    const defaultOption = document.createElement('option');
+    defaultOption.innerText = "default";
+    defaultOption.value = "null";
+    selectSampleGame.add(defaultOption);
+
+    buttonValidateInput.addEventListener('click', () => {
+        if (inputGridSize.validity.valid &&
+            inputStart.validity.valid &&
+            inputGoal.validity.valid &&
+            inputNonsteppables.validity.valid) {
+            const gameInput = validateInput(inputGridSize.value, inputStart.value, inputGoal.value, inputNonsteppables.value);
+            if (!gameInput.ok) {
+                messageArea.innerText = `Status: Input is invalid.
+                    ${gameInput.message}`;
+
+                buttonFindPaths.disabled = true;
+
+                return;
+            } else {
+                messageArea.innerText = `Status: Input is valid.
+                    Grid size: ${gameInput.w} x ${gameInput.h}
+                    Start: ${gameInput.start.x}, ${gameInput.start.y}
+                    Goal: ${gameInput.goal.x}, ${gameInput.goal.y}
+                    Nonsteppables: ${gameInput.nonsteppables.map(({ x, y }) => `(${x}, ${y})`).join(" ")}`;
+
+                clearGrid(gridEl);
+                renderGrid(gameInput.w, gameInput.h, gridEl, [gameInput.start, gameInput.goal], gameInput.nonsteppables, gameInput.start, gameInput.goal);
+
+                const findPathHandler = () => {
+                    clearGrid(document.body);
+                    const result_paths = findPaths(gameInput.w, gameInput.h, gameInput.start, gameInput.goal, gameInput.nonsteppables, Number(selectSearchVariant.value));
+                    for (const path of result_paths) {
+                        renderGrid(gameInput.w, gameInput.h, gridEl, path, gameInput.nonsteppables, gameInput.start, gameInput.goal);
+                    }
+                    buttonFindPaths.removeEventListener('click', findPathHandler);
+                    buttonFindPaths.disabled = true;
+                }
+                buttonFindPaths.addEventListener('click', findPathHandler);
+                buttonFindPaths.disabled = false;
+                return;
+            }
+        } else {
+            messageArea.innerText = `Validation Status:
+                Grid size : ${inputGridSize.validationMessage}
+                Start : ${inputStart.validationMessage}
+                Goal : ${inputGoal.validationMessage}
+                Nonsteppables : ${inputNonsteppables.validationMessage}`;
+            buttonFindPaths.disabled = true;
+
+            return;
+        }
+    });
+
+    parentEl.appendChild(buttonClearGrids);
+    parentEl.appendChild(buttonValidateInput);
+    parentEl.appendChild(buttonFindPaths);
+    parentEl.appendChild(labelGridSize);
+    parentEl.appendChild(inputGridSize);
+    parentEl.appendChild(labelStart);
+    parentEl.appendChild(inputStart);
+    parentEl.appendChild(labelGoal);
+    parentEl.appendChild(inputGoal);
+    parentEl.appendChild(labelNonsteppables);
+    parentEl.appendChild(inputNonsteppables);
+    parentEl.appendChild(labelSelectSearchVariant);
+    parentEl.appendChild(selectSearchVariant);
+    parentEl.appendChild(labelSelectSampleGame);
+    parentEl.appendChild(selectSampleGame);
+    parentEl.appendChild(messageArea);
+
+    return {
+        inputGridSize,
+        inputStart,
+        inputGoal,
+        inputNonsteppables,
+        selectSampleGame,
+    };
+};
+
+const initializeUi = () => {
+    const uiEl = document.createElement('div');
+    const gridsContainerEl = document.createElement('div');
+    gridsContainerEl.classList.add('grid-container');
+    document.body.appendChild(uiEl);
+    document.body.appendChild(gridsContainerEl);
+
+    const inputs = renderUserInterface(uiEl, gridsContainerEl);
+
+    return {
+        ui: uiEl,
+        gridsContainer: gridsContainerEl,
+        registerSampleGameInput: (title, w, h, nonsteppables, start, goal) => {
+            const optionSampleGame = document.createElement('option');
+            optionSampleGame.innerText = title;
+            optionSampleGame.value = JSON.stringify({
+                title,
+                w, h,
+                nonsteppables,
+                start,
+                goal,
+            });
+            inputs.selectSampleGame.add(optionSampleGame);
+        },
+    };
+}
